b3c32a7ff0d249c0cc8ca8055db9297a84946858
Antonesei Cristian <antoneseicristian96@gmail.com>
2023-07-07 16:22:47 +0300

    v0.2
    Title: Implement User Authentication and BMI Calculation
    
    Description:
    
    This commit introduces several major features to the BMI App:
    
    1. **User Authentication**: Users can now create accounts and log in. The application uses Flask-Login for handling user sessions. Passwords are hashed using Werkzeug's security utilities. The login page is set as the landing page for the application.
    
    2. **BMI Calculation**: Authenticated users can calculate their Body Mass Index (BMI) by entering their weight and height. The application calculates the BMI and provides recommendations based on the BMI value.
    
    3. **Internationalization**: The application supports multiple languages (English and Romanian). Users can switch between languages using the language selector on the web page. Flask-Babel is used for internationalization.
    
    4. **Database Integration**: SQLite is used as the database for storing user information. Flask-SQLAlchemy is used as the ORM.
    
    5. **Styling**: Basic styling is applied to the web pages using CSS to improve the user interface.
    
    6. **Error Handling**: Proper error messages are displayed for login failures.
    
    7. **Code Documentation**: Comments and documentation have been added to the `app.py` file to make the code more understandable to other developers.
    
    This commit lays the foundation for a multi-featured BMI application with user authentication and internationalization support. Future improvements can include more detailed health recommendations, user profiles, and data visualization.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_babel import Babel, _
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bmi_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'landing'  # Updated to 'landing'
+
 babel = Babel()
 
 def get_locale():
@@ -10,12 +21,61 @@ def get_locale():
 
 babel.init_app(app, locale_selector=get_locale)
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
 @app.route('/setlang/<lang>')
 def setlang(lang):
     session['lang'] = lang
     return redirect(url_for('index'))
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/landing', methods=['GET', 'POST'])
+def landing():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        flash('Login failed. Check your username and/or password.', 'danger')
+    return render_template('landing.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        hashed_password = generate_password_hash(password, method='sha256')
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful. You can now log in.', 'success')
+        return redirect(url_for('landing'))
+    return render_template('register.html')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('landing'))
+
+@app.route('/')
+@login_required
+def home():
+    return redirect(url_for('landing'))
+
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
     bmi = ''
     recommendation = ''
