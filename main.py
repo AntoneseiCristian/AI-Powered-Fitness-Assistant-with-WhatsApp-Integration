b5f6174afd6b8c9b8aa8e59b2c763403027dd54b
Antonesei Cristian <antoneseicristian96@gmail.com>
2023-07-07 13:23:38 +0300

    v0.1

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    bmi = ''
+    if request.method == 'POST' and 'weight' in request.form and 'height' in request.form:
+        weight = float(request.form.get('weight'))
+        height = float(request.form.get('height'))
+        bmi = calculate_bmi(weight, height)
+    return render_template('index.html', bmi=bmi)
+
+def calculate_bmi(weight, height):
+    return round(weight / ((height / 100) ** 2), 2)
+
+if __name__ == "__main__":
+    app.run(debug=True)
