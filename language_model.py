485b4c95bdcd84591ca62952dfba6482cea934a6
Antonesei Cristian <antoneseicristian96@gmail.com>
2023-07-17 17:36:48 +0300

    v0.51

--- language_model.py
+++ language_model.py
@@ -1,7 +1,9 @@
 from gpt4all import GPT4All
 
-model = GPT4All("C:/Users/Antonesei/AppData/Local/nomic.ai/GPT4All/ggml-model-gpt4all-falcon-q4_0.bin")
+model = GPT4All("C:/Users/Antonesei/AppData/Local/nomic.ai/GPT4All/nous-hermes-13b.ggmlv3.q4_0")
 
 def get_response(prompt):
-    output = model.generate(prompt, max_tokens=3)
+    output = model.generate(prompt, max_tokens=200, temp=0.7, top_k=40, top_p=0.1, repeat_penalty=1.18, repeat_last_n=64, n_batch=8, n_predict=None, streaming=False)
+    if output.startswith(","):
+        output = output[1:].strip()
     return output
