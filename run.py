48c6fcde734c23555cc5dd41121c18578dff1413
Antonesei Cristian <antoneseicristian96@gmail.com>
2023-07-28 13:42:15 +0300

    v0.63
    Title: Add Twilio account creation and ngrok setup to registration process
    
    Body:
    This commit introduces two major changes to the registration process:
    
    1. Twilio Account Creation: When the user clicks the "Register" button, they are now redirected to the Twilio login page in a new tab. This is necessary because our application relies on Twilio for WhatsApp integration, and users need to have a Twilio account to use this feature. We used JavaScript to open the Twilio login page in a new tab when the form is submitted, ensuring that the form data is still sent to our server.
    
    2. ngrok Setup: We added code to start an ngrok tunnel when the application starts. This is necessary because our application needs to be accessible from the internet for the Twilio integration to work. The ngrok tunnel forwards incoming requests to our local server, making it accessible from the internet.
    
    These changes improve the user experience by guiding them through the necessary setup steps for using our application.

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
+from pyngrok import ngrok
 
 app = create_app()
 
 if __name__ == "__main__":
+    public_url = ngrok.connect(5000)
+    print("Public URL:", public_url)
     app.run(debug=True)
--- whatsapp_message.py
+++ whatsapp_message.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 
 def send_whatsapp_message(message):
     account_sid = 'ACa96dca1ec5a81f61bd6e93a7e8d74bfd'
-    auth_token = 'e41b2ff769037315efc7943008d76261'
+    auth_token = '@'
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
